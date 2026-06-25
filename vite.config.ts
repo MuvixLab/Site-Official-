f7bd1df29119a6ffdd928a273949d7ec0bf61dd5
Miguel Augusto <miguelpecanha2019@gmail.com>
2026-06-25 19:12:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: "vercel",
+  },
+
   tanstackStart: {
     server: {
       entry: "server",
-    },
-    nitro: {
-      preset: "vercel",
     },
   },
 });
